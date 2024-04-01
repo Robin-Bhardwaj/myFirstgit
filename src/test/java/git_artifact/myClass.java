1ce073db5ac065a1383e3219b372be8e6b4c8306
Robin <bhardwajrobin771@gmail.com>
2024-04-01 23:23:17 +0530

    First commit / Apr 1, 2024

---
+++ src/test/java/git_artifact/myClass.java
@@ -0,0 +1,16 @@
+package git_artifact;
+
+public class myClass {
+
+	public static void main(String[] args) {
+		
+			   
+			        int number = 10;
+			        String text = "Hello, Java!";
+			        
+			        System.out.println("Number: " + number);
+			        System.out.println("Text: " + text);	   
+
+	}
+
+}
